a96aa5b21c2230b9afce5f545e30025c68439720
ValentinVIL <emil.kiesche@gmail.com>
2023-06-29 01:47:40 +0200

    Some fish and one with animation

---
+++ Assets/Scripts/ElevatorMoving.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorMoving : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y - 0.001f, transform.position.z);
+    }
+}
