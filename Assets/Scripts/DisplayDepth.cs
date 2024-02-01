2b39c7bb04e17c1ca979c7239f51dc37744c2a32
ValentinVIL <emil.kiesche@gmail.com>
2024-02-02 00:42:06 +0100

    Some upgrades

--- Assets/Scripts/DisplayDepth.cs
+++ Assets/Scripts/DisplayDepth.cs
@@ -14,10 +14,20 @@ public class DisplayDepth : MonoBehaviour
 
     private void Update()
     {
-        
-        // Get the player's Y-coordinate and round it
-        int roundedY = Mathf.RoundToInt(Mathf.Abs(playerTransform.position.y));
+        // Bestimmen der Tiefe basierend auf dem Y-Wert des Spielers
+        int depth;
+        if (playerTransform.position.y > 0)
+        {
+            // Wenn der Y-Wert positiv ist, Tiefe als 0 anzeigen
+            depth = 0;
+        }
+        else
+        {
+            // Wenn der Y-Wert negativ ist, den absoluten Wert für die Tiefe verwenden
+            depth = Mathf.RoundToInt(Mathf.Abs(playerTransform.position.y));
+        }
+
         // Update the text display
-        textMesh.text = $"{roundedY} \nMeter tief";
+        textMesh.text = $"{depth} \nMeter tief";
     }
 }
